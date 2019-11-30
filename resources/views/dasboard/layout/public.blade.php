4e824db534e98e4d8efabbac0c1ed59173c8f412
mosinagaz <mosinaga@desarrollamelo.com>
2019-11-30 00:45:39 -0400

    scrips

--- resources/views/dasboard/layout/public.blade.php
+++ resources/views/dasboard/layout/public.blade.php
@@ -270,28 +270,28 @@
     <!-- END: Footer-->
    
     <!-- BEGIN: Vendor JS-->
-    <script src="app-assets/vendors/js/vendors.min.js"></script>
-    <script src="app-assets/vendors/js/forms/select/select2.full.min.js"></script>
-    <script src="app-assets/vendors/js/ui/prism.min.js"></script>
-    <script src="app-assets/vendors/js/extensions/wNumb.js"></script>
-    <script src="app-assets/vendors/js/extensions/nouislider.min.js"></script>
-    <script src="app-assets/vendors/js/forms/select/select2.full.min.js"></script>
+    <script src="{{ asset('app-assets/vendors/js/vendors.min.js') }}"></script>
+    <script src="{{ asset('app-assets/vendors/js/forms/select/select2.full.min.js') }}"></script>
+    <script src="{{ asset('app-assets/vendors/js/ui/prism.min.js') }}"></script>
+    <script src="{{ asset('app-assets/vendors/js/extensions/wNumb.js') }}"></script>
+    <script src="{{ asset('app-assets/vendors/js/extensions/nouislider.min.js') }}"></script>
+    <script src="{{ asset('app-assets/vendors/js/forms/select/select2.full.min.js') }}"></script>
     <!-- BEGIN Vendor JS-->
 
     <!-- BEGIN: Page Vendor JS-->
-    <script src="app-assets/vendors/js/ui/prism.min.js"></script>
-    <script src="app-assets/js/core/app-menu.js"></script>
-    <script src="app-assets/js/core/app.js"></script>
-    <script src="app-assets/js/scripts/components.js"></script>
+    <script src="{{ asset('app-assets/vendors/js/ui/prism.min.js') }}"></script>
+    <script src="{{ asset('app-assets/js/core/app-menu.js') }}"></script>
+    <script src="{{ asset('app-assets/js/core/app.js') }}"></script>
+    <script src="{{ asset('app-assets/js/scripts/components.js') }}"></script>
     <!-- END: Page Vendor JS-->
 
     <!-- BEGIN: Theme JS-->
-    <script src="app-assets/js/core/app-menu.js"></script>
-    <script src="app-assets/js/core/app.js"></script>
+    <script src="{{ asset('app-assets/js/core/app-menu.js') }}"></script>
+    <script src="{{ asset('app-assets/js/core/app.js') }}"></script>
     <!-- END: Theme JS-->
 
     <!-- BEGIN: Page JS-->
-    <script src="app-assets/js/scripts/pages/app-ecommerce-shop.js"></script>
+    <script src="{{ asset('app-assets/js/scripts/pages/app-ecommerce-shop.js') }}"></script>
     <!-- END: Page JS-->
     @stack('styles')
     @stack('scripts')
